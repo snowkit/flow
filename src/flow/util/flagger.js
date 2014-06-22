5882b6f2519311f27839d99b871b9d4edec733dd
underscorediscovery <sven@underscorediscovery.com>
2014-06-22 16:13:56 -0230

    fixing fail on no arguments

--- src/flow/util/flagger.js
+++ src/flow/util/flagger.js
@@ -28,7 +28,7 @@ module.exports = {
 
         flags._alias = function _alias(flag) {
                 //strip internal commands
-            if(flag.charAt(0) != '_') {
+            if(flag && flag.charAt(0) != '_') {
                 return this._aliases[flag] || flag;
             }
         }
