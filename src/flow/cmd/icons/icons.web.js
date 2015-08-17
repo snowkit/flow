2c7fb41698364f1b10ed9afc64d5f281ca74919d
underscorediscovery <sven@underscorediscovery.com>
2015-08-17 09:06:59 -0230

    icons; web; warn about missing icons but don't error

--- src/flow/cmd/icons/icons.web.js
+++ src/flow/cmd/icons/icons.web.js
@@ -2,6 +2,7 @@
 var   util = require('../../util/util')
     , cmd = require('../../util/process')
     , path = require('path')
+    , fs = require('fs-extra')
 
 exports.convert = function(flow, icon, done) {
 
@@ -12,8 +13,16 @@ exports.convert = function(flow, icon, done) {
     var icon_file = 'favicon.png';
     var icon_output = path.join(flow.project.paths.files, icon_file);
 
-    flow.log(3,'icons - ok - copying to output folder');
-    util.copy_path(flow, icon_source, icon_output);
+    if(fs.existsSync(icon_source)) {
+
+        flow.log(3,'icons - ok - copying to output folder');
+        util.copy_path(flow, icon_source, icon_output);
+
+    } else {
+
+        flow.log(2,'icons - warning - icons node specified but no source file for web at %s', icon_source);
+
+    }
 
     if(done) {
         done();
