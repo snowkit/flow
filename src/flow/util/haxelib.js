96263261a07d7dc5a65276d3258b1dea77c06c61
Kevin Leung <kevinresol@users.noreply.github.com>
2015-11-09 21:48:28 +0800

    patch git version as well

--- src/flow/util/haxelib.js
+++ src/flow/util/haxelib.js
@@ -194,7 +194,7 @@ internal.parse_versions = function(flow) {
                 v = 'dev';
             } else if(v == 'git') {
                     //git folder is in the root like so
-                lib_path = path.join( exports.haxelib_path, lib, 'git' );
+                lib_path = path.join( exports.haxelib_path, lib.replace(/\./gi,','), 'git' );
             } else {
                     //the other types becomes /v,e,r/
                 lib_path = path.join( exports.haxelib_path, lib.replace(/\./gi,','), v.replace(/\./gi,',') );
