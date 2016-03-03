bb19497e10c123c6e05d2b812ffc444fd5ecf44f
underscorediscovery <sven@underscorediscovery.com>
2016-03-03 10:19:56 -0330

    flags; ensure dependency order is retained in hxml

--- src/flow/project/prepare/flags.js
+++ src/flow/project/prepare/flags.js
@@ -6,19 +6,41 @@ exports.parse = function parse(flow, prepared) {
 
     var flags = prepared.flags || [];
 
-            //the default flags set
-        if(prepared.source.project.build.flags) {
-            flags = util.array_union(flags, prepared.source.project.build.flags);
+            //handle dependencies in order
+        for(index in prepared.depends_list) {
+
+            var name = prepared.depends_list[index];
+            var depend = prepared.depends[name];
+
+            if(depend.project.project.build.flags) {
+                flags = util.array_union(flags, depend.project.project.build.flags);
+            }
+
+            for(condition in depend.project.if) {
+                var node = depend.project.if[condition];
+                if(node.build && node.build.flags) {
+                    if(defines.satisfy(flow, prepared, condition)) {
+                        flags = util.array_union(flags, node.build.flags);
+                    }
+                }
+            } //each condition
+
+        } //name
+
+            //the project itself
+        var parsed = flow.project.parsed;
+        if(parsed.project.build.flags) {
+            flags = util.array_union(flags, parsed.project.build.flags);
         }
 
-        for(condition in prepared.source.if) {
-            var node = prepared.source.if[condition];
+        for(condition in parsed.if) {
+            var node = parsed.if[condition];
             if(node.build && node.build.flags) {
                 if(defines.satisfy(flow, prepared, condition)) {
                     flags = util.array_union(flags, node.build.flags);
                 }
             }
-        }
+        } //each condition
 
     return flags;
 
